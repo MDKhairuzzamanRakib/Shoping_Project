bc9e830304eb58f935b41862e3a53224e64a9764
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2023-11-04 02:36:30 +0600

    Test Controller

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace work_Session.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var s = HttpContext.Session.GetString("r56");
+            ViewBag.msg = s;
+            return View();
+        }
+    }
+}
